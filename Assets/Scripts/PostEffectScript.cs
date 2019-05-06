5078c7714e3f25fbbb1c44cab78274bee04085eb
COMPJB-000096\jbUser <sergey.prokofiev@jetbi.com>
2019-05-06 16:47:58 +0300

    Initial commit

---
+++ Assets/Scripts/PostEffectScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffectScript : MonoBehaviour
+{
+    public Material mat;
+
+
+    void Update()
+    {
+        Debug.Log("AAAAAAAAA");
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        //src is the fully rendered scene that you normally send to the monitore but we intercepting it 
+        //to do more work before passing it on.
+
+        Graphics.Blit(src, dest, mat);
+    }
+
+
+}
